c53be8d44c92f4f3d02a65e3293d71e62d9362c1
Oversv <albertomdv@gmail.com>
2020-02-13 17:48:16 +0000

    Updates

---
+++ precache-manifest.a273ef620cf8e29accd3611d191ee946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb7f48157dd91bd40e69e268fbabb92",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/index.html"
+  },
+  {
+    "revision": "846ae55dce42010b8d08",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/static/css/main.491cf38b.chunk.css"
+  },
+  {
+    "revision": "5ea04c5e2a0e4af9dc16",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/static/js/2.45b3848d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/static/js/2.45b3848d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846ae55dce42010b8d08",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/static/js/main.2fc169e5.chunk.js"
+  },
+  {
+    "revision": "7d99b44243b7bb31e545",
+    "url": "/oversv/freeCodeCamp-Random_Quote_Machine.git/static/js/runtime-main.08c4d0c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Oversv/freeCodeCamp-Random_Quote_Machine.git/precache-manifest.015cd113570445aa914ff16063125b94.js"
+  "/oversv/freeCodeCamp-Random_Quote_Machine.git/precache-manifest.a273ef620cf8e29accd3611d191ee946.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Oversv/freeCodeCamp-Random_Quote_Machine.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oversv/freeCodeCamp-Random_Quote_Machine.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
